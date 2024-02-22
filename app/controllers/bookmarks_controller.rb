aef767f4f26551c1c4320ec6dd5f4dbc94ab5b41
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-22 20:20:21 +0000

    Completed the initial functionalities and formatting of the application

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    redirect_list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(redirect_list), status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
