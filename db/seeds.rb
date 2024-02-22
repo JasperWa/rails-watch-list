8d9340a3271378af60a2040b19b49fedb2f66f0f
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-22 12:55:54 +0000

    Added initial features for lists (index, show, create, new)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database'
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+
+puts 'Initialising API...'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
+
+puts "Adding #{user['results'].length} movies retrieved from API..."
+
+user['results'].each do |movie|
+  title = movie['original_title']
+  puts "Adding #{title}..."
+  overview = movie['overview']
+  poster_url = movie['poster_path']
+  rating = movie['vote_average'].round(1)
+
+  movie = Movie.new(title:, overview:, poster_url:, rating:)
+  movie.save
+end
