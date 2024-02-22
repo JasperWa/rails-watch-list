0ac0cfbfe3b3df5dadc8db5b09063c5085f54d76
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-22 11:55:44 +0000

    Included associations and validations in models (movie, list, bookmark)

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  # Validations
+  validates :movie_id, :list_id, presence: true
+  validates :movie_id, uniqueness: { scope: [:list_id] }
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+  before_destroy :check_bookmarks
+  has_many :bookmarks
+
+  # Validations
+  validates :title, :overview, presence: true
+  validates :title, uniqueness: true
+
+  private
+
+  def check_bookmarks
+    Bookmark.where(movie: self).empty?
+  end
 end
