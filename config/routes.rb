ffa978397ba15d62d62fc11bbf2edf0574c7941d
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-23 08:51:47 +0000

    Added Movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  resources :movies, only: %i[index]
+
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
     resources :reviews, only: %i[create]
