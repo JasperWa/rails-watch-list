1d93d614fffe67016bb2840c2fcf85eb43802aad
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-23 16:08:21 +0000

    Updated app for users to upload photos for lists which are then displayed

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,4 +7,18 @@ class List < ApplicationRecord
   # Validations
   validates :name, uniqueness: true
   validates :name, presence: true
+  validate :photo_presence # Customer validation for photos
+
+  private
+
+  # Validates the presence and file type of a photo
+  def photo_presence
+    if photo.attached?
+      unless photo.content_type.in?(%w[image/jpeg image/png image/gif])
+        errors.add(:photo, 'must be a JPEG, PNG, or GIF')
+      end
+    else
+      errors.add(:photo, 'must be attached')
+    end
+  end
 end
